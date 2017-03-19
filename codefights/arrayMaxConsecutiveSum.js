97a21ac258349e5c29080a3cff86351f2ce86bc8
anand mutyala <mutyala2011@gmail.com>
2017-03-19 12:13:51 +0530

    improved time complexity to O(n)

--- codefights/arrayMaxConsecutiveSum.js
+++ codefights/arrayMaxConsecutiveSum.js
@@ -13,23 +13,17 @@
 // Thus, the answer is 8.
 
 function arrayMaxConsecutiveSum(inputArray, k) {
-    var max = -Infinity;
-    // This solution exceeded the time limit of 4000ms in one of the hidden test cases :p
-    // for(var i=0;i<=inputArray.length-k;i++) {
-    //     var partialSum = inputArray.slice(i,i+k).reduce((sum,int)=>sum+int,0); 
-    //     if(max <= partialSum) {
-    //         max = partialSum;
-    //     }
-    // }
-    var partialSum = 0;
-    for(var i=0;i<=inputArray.length-k;i++) {
-        for(var j=i;j<i+k;j++) {
-            partialSum+=inputArray[j];
-        }
-        if(max <= partialSum) {
-            max = partialSum;
-        }
-        partialSum = 0;
+    var max;
+    var nextSum;
+    for(var i=0;i<k;i++) {
+        nextSum += inputArray[i];
+    }
+    max = nextSum;
+    for(var i=k;i<inputArray.length;i++) {
+        nextSum -= inputArray[i-k];
+        nextSum += inputArray[k];
+        if(nextSum >= max)
+            max = nextSum;
     }
     return max;
 }
