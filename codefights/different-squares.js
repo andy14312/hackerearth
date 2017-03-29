4ca68e017f248716548ead78c5e0a70dbe44729f
anand mutyala <mutyala2011@gmail.com>
2017-03-29 23:20:10 +0530

    unlocked level core

--- codefights/different-squares.js
+++ codefights/different-squares.js
@@ -28,19 +28,41 @@
 // 2 1
 
 function differentSquares(matrix) {
-    var count = 1;
-    for(var i=0;i<matrix.length-1;i++) {
-        for(var j=0;j<matrix[i].length-1;j++) {
-            if(!(matrix[i][j] === matrix[i][j+1] && matrix[i+1][j] === matrix[i+1][j+1] && matrix[i][j] === matrix[i+1][j]))
-                count++;
+    // var count = 1;
+    // var squares = [];
+    // var square = [];
+    // var diff = {};
+    // var sumOfdiff = 0;
+    // for(var i=0;i<matrix.length-1;i++) {
+    //     for(var j=0;j<matrix[i].length-1;j++) {
+    //         square = [matrix[i][j],matrix[i][j+1],matrix[i+1][j],matrix[i+1][j+1]];
+    //         squares.push(square.join(""));
+    //     }
+    // }
+    // for(var i=0;i<squares.length;i++) {
+    //     if(!diff[squares[i]]) {
+    //         diff[squares[i]] = 1;
+    //     }
+    // }
+    // for(key in diff) {
+    //     sumOfdiff += diff[key];
+    // }
+    // return sumOfdiff;
+    var a = matrix;
+    var s = new Set;
+    for( var i=0; i<a.length-1; i++ ) {
+        for( var j=0; j<a[0].length-1; j++ ) {
+            var hash = a[i][j] + 10*a[i][j+1] + 100*a[i+1][j] + 1000*a[i+1][j+1];
+            console.log(hash);
+            s.add( hash );
         }
     }
-    return count;
+    
+    return s.size;
 }
 
-console.log(differentSquares([[9,9,9,9,9], 
-                              [9,9,9,9,9], 
-                              [9,9,9,9,9], 
-                              [9,9,9,9,9], 
-                              [9,9,9,9,9], 
-                              [9,9,9,9,9]]));
+console.log(differentSquares([[1, 2, 1],
+                              [2, 2, 2],
+                              [2, 2, 2],
+                              [1, 2, 3],
+                              [2, 2, 1]]));
